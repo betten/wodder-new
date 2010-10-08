38e51e9a383ffef7f93ff01ba43631c161bca818
betten <betten@bc.edu>
2010-10-08 18:46:33 +0000

    add signup skeleton code
    Added signup skeleton code to users controller for user signup.  Still to be completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def signup
+    if request.post?
+      u = User.new(
+        :email => params[:email],
+        :password => params[:password],
+        :password_confirmation => params[:password_confirmation])
+      if u.save
+        # set session
+        # redirect_to user home
+      else
+        flash[:errors] = u.errors
+        # redirect_to signup
+    end
   end
 
   def signin
