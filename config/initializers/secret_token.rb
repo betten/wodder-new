22cd4d01901ec775caab564a74173bf229c1978c
betten <betten@bc.edu>
2010-10-08 15:28:24 +0000

    new wodder first commit
    Rails 3, mongoid, resque good to go.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WodderNew::Application.config.secret_token = 'bafce721203479849089a3e6b2fe63e215f83ee538a2a172bdf3c1800780afef20d81ec6885894e5c04b5ec29366e77b3192badf39d8540219434eed68a6effc'
---
+++ vendor/plugins/resque/lib/resque/version.rb
@@ -0,0 +1,3 @@
+module Resque
+  Version = VERSION = '1.11.0'
+end
