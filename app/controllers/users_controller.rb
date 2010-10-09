0f8ce0b007242e76ff4b3ac20f48a112a2a116fb
betten <betten@bc.edu>
2010-10-09 03:21:04 +0000

    updated user signup signin
    Added basic user signup signin code, still to be tested.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,33 @@ class UsersController < ApplicationController
         :password => params[:password],
         :password_confirmation => params[:password_confirmation])
       if u.save
-        # set session
-        # redirect_to user home
+        session[:user] = u
+        redirect_to '/users/home'
       else
         flash[:errors] = u.errors
-        # redirect_to signup
     end
   end
 
   def signin
+    if request.post?
+      u = User.authenticate(params[:email], params[:password])
+      if u.nil?
+        flash[:errors] = 'invalid email password combo'
+      else
+        session[:user] = u
+        redirect_to '/users/home'
+      end
+    end
   end
 
   def signout
+    session[:user] = nil
   end
 
   def view
+  end
+
+  def home
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,41 @@
+require 'digest/md5'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+
   validates_presence_of :email, :password
   validates_uniqueness_of :email
   validates_format_of :email, :with => /[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/
   validates_length_of :password, :within => 4..20
   validates_confirmation_of :password
+
   field :email
   field :password
+  field :salt
+
   embeds_many :saved_wods
   referenced_in :user_wod
 
+  before_save :hash_password  
+
+  def self.authenticate(email, password)
+    u = User.first(:conditions => { :email => email })
+    return u if u.password == hash(password, u.salt)
+    return nil
+  end
+
+  protected
+
+  def hash(password, salt)
+    return Digest::MD5.hexdigest(password + salt)
+  end
+
+  def generate_salt
+    return Time.now.to_s # a better salt most likely exists
+  end
+
+  def hash_password
+    self.password = hash(self.password, generate_salt) if self.password_changed?
+  end
 end
