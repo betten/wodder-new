b279e73c486783eed3843ef8ae8fbbcdc1d64904
betten <betten@bc.edu>
2011-01-05 17:08:43 -0500

    updated gyms and users list views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,11 @@ module ApplicationHelper
     return text
   end
 
+  # see pluralize source at
+  # http://api.rubyonrails.org/classes/ActionView/Helpers/TextHelper.html#method-i-pluralize
+  # a method to pluralize just the given word was needed
+  def pluralize_word(count, singular, plural = nil)
+    (count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize)
+  end
+
 end
